ab2152b2f763f51597c019e554df562d606a7068
Muhammad Rendra <muhammadnurrendra@gmail.com>
2019-12-03 04:06:35 +0700

    init main-component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import './App.css'
 import Header from './components/Header'
+import Main from './components/Main'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   )
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './style.css'
+
+const Header = () => {
+  return <div className="main">Main</div>
+}
+
+export default Header
