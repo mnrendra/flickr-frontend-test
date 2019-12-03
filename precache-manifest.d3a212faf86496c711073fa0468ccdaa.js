9564b0d4de56472a086ad78a8bd0f2864386db82
Muhammad Rendra <muhammadnurrendra@gmail.com>
2019-12-03 11:57:40 +0700

    Updates

---
+++ precache-manifest.d3a212faf86496c711073fa0468ccdaa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2468fe674998f9c1d29e185d59c28a2",
+    "url": "/flickr-frontend-test/index.html"
+  },
+  {
+    "revision": "b9b59bda8688128cf2ee",
+    "url": "/flickr-frontend-test/static/css/main.b59a49db.chunk.css"
+  },
+  {
+    "revision": "b0daf88319184e9b3813",
+    "url": "/flickr-frontend-test/static/js/2.520f80f4.chunk.js"
+  },
+  {
+    "revision": "b9b59bda8688128cf2ee",
+    "url": "/flickr-frontend-test/static/js/main.f05851b4.chunk.js"
+  },
+  {
+    "revision": "268ee98385d333aa917e",
+    "url": "/flickr-frontend-test/static/js/runtime-main.71cace97.js"
+  },
+  {
+    "revision": "cea2bf042d83c835aaaa86accaf45a32",
+    "url": "/flickr-frontend-test/static/media/flickr-loading.cea2bf04.gif"
+  },
+  {
+    "revision": "b9c8ddfc05ad9301c69ed353456ba17b",
+    "url": "/flickr-frontend-test/static/media/flickr.b9c8ddfc.png"
+  },
+  {
+    "revision": "3bd2600df5b5b7914fb817f2344fb5df",
+    "url": "/flickr-frontend-test/static/media/proxima-nova-light.3bd2600d.woff2"
+  },
+  {
+    "revision": "c07517329a30d7a4d303f797171c7def",
+    "url": "/flickr-frontend-test/static/media/proxima-nova-regular.c0751732.woff2"
+  },
+  {
+    "revision": "9b6f2d8ae1e4e8dec8ee1cde27e3259a",
+    "url": "/flickr-frontend-test/static/media/proxima-nova-semibold.9b6f2d8a.woff2"
+  }
+]);
