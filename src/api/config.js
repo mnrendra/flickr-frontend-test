26db9184e344b66c771671cafa452ed9393ed5f0
Muhammad Rendra <muhammadnurrendra@gmail.com>
2019-12-03 12:24:24 +0700

    change URL to my digitalocean back-end

--- src/api/config.js
+++ src/api/config.js
@@ -1,1 +1,4 @@
-export const URL = 'http://ec2-54-179-136-166.ap-southeast-1.compute.amazonaws.com:4000'
+// AWS - EC2 Intance
+// export const URL = 'http://ec2-54-179-136-166.ap-southeast-1.compute.amazonaws.com:4000'
+// Digital Ocean
+export const URL = 'http://178.128.88.151:4000'
